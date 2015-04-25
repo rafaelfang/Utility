246589b7cc64e4376e860dab2883a1805a688735
rafaelfang <cf797@mail.missouri.edu>
2015-04-25 13:10:54 -0500

    HHRResult

---
+++ src/HHRResult.cpp
@@ -0,0 +1,176 @@
+/*
+ * HHRResult.cpp
+ *
+ *  Created on: Apr 25, 2015
+ *      Author: cf797
+ */
+
+#include "HHRResult.h"
+
+HHRResult::HHRResult() {
+	// TODO Auto-generated constructor stub
+
+}
+int HHRResult::getAlignedCols() {
+	return aligned_cols;
+}
+
+void HHRResult::setAlignedCols(int alignedCols) {
+	aligned_cols = alignedCols;
+}
+
+string& HHRResult::getAlignment() {
+	return alignment;
+}
+
+void HHRResult::setAlignment(string& alignment) {
+	this->alignment = alignment;
+}
+
+string& HHRResult::getConfidence() {
+	return confidence;
+}
+
+void HHRResult::setConfidence(string& confidence) {
+	this->confidence = confidence;
+}
+
+float HHRResult::getExpect() {
+	return expect;
+}
+
+void HHRResult::setExpect(float expect) {
+	this->expect = expect;
+}
+
+int HHRResult::getIdentities() {
+	return identities;
+}
+
+void HHRResult::setIdentities(int identities) {
+	this->identities = identities;
+}
+
+float HHRResult::getProbab() {
+	return probab;
+}
+
+void HHRResult::setProbab(float probab) {
+	this->probab = probab;
+}
+
+string& HHRResult::getProteinName() {
+	return proteinName;
+}
+
+void HHRResult::setProteinName(string& proteinName) {
+	this->proteinName = proteinName;
+}
+
+string& HHRResult::getQuery() {
+	return query;
+}
+
+void HHRResult::setQuery(string& query) {
+	this->query = query;
+}
+
+string& HHRResult::getQueryConsensus() {
+	return queryConsensus;
+}
+
+void HHRResult::setQueryConsensus(string& queryConsensus) {
+	this->queryConsensus = queryConsensus;
+}
+
+int HHRResult::getQueryEnd() {
+	return queryEnd;
+}
+
+void HHRResult::setQueryEnd(int queryEnd) {
+	this->queryEnd = queryEnd;
+}
+
+int HHRResult::getQueryStart() {
+	return queryStart;
+}
+
+void HHRResult::setQueryStart(int queryStart) {
+	this->queryStart = queryStart;
+}
+
+float HHRResult::getScore() {
+	return score;
+}
+
+void HHRResult::setScore(float score) {
+	this->score = score;
+}
+
+int HHRResult::getSimilarities() {
+	return similarities;
+}
+
+void HHRResult::setSimilarities(int similarities) {
+	this->similarities = similarities;
+}
+
+float HHRResult::getSumProbs() {
+	return sum_probs;
+}
+
+void HHRResult::setSumProbs(float sumProbs) {
+	sum_probs = sumProbs;
+}
+
+string& HHRResult::getTarget() {
+	return target;
+}
+
+void HHRResult::setTarget(string& target) {
+	this->target = target;
+}
+
+string& HHRResult::getTargetSsPred() {
+	return target_ss_pred;
+}
+
+void HHRResult::setTargetSsPred(string& targetSsPred) {
+	target_ss_pred = targetSsPred;
+}
+
+string& HHRResult::getTargetConsensus() {
+	return targetConsensus;
+}
+
+void HHRResult::setTargetConsensus(string& targetConsensus) {
+	this->targetConsensus = targetConsensus;
+}
+
+int HHRResult::getTargetEnd() {
+	return targetEnd;
+}
+
+void HHRResult::setTargetEnd(int targetEnd) {
+	this->targetEnd = targetEnd;
+}
+
+int HHRResult::getTargetStart() {
+	return targetStart;
+}
+
+void HHRResult::setTargetStart(int targetStart) {
+	this->targetStart = targetStart;
+}
+
+string& HHRResult::getQuerySsPred() {
+	return query_ss_pred;
+}
+
+void HHRResult::setQuerySsPred(string& querySsPred) {
+	query_ss_pred = querySsPred;
+}
+HHRResult::~HHRResult() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/HHRResult.h
@@ -0,0 +1,83 @@
+/*
+ * HHRResult.h
+ *
+ *  Created on: Apr 25, 2015
+ *      Author: cf797
+ */
+
+#ifndef HHRRESULT_H_
+#define HHRRESULT_H_
+
+#include <string>
+
+#include "AlignmentResult.h"
+
+class HHRResult :public AlignmentResult{
+public:
+	HHRResult();
+	virtual ~HHRResult();
+	int getAlignedCols();
+	void setAlignedCols(int alignedCols);
+	string& getAlignment();
+	void setAlignment(string& alignment);
+	string& getConfidence();
+	void setConfidence(string& confidence);
+	float getExpect();
+	void setExpect(float expect);
+	int getIdentities();
+	void setIdentities(int identities);
+	float getProbab();
+	void setProbab(float probab);
+	string& getProteinName();
+	void setProteinName(string& proteinName);
+	string& getQuery();
+	void setQuery(string& query);
+	string& getQueryConsensus();
+	void setQueryConsensus(string& queryConsensus);
+	int getQueryEnd();
+	void setQueryEnd(int queryEnd);
+	int getQueryStart();
+	void setQueryStart(int queryStart);
+	float getScore();
+	void setScore(float score);
+	int getSimilarities();
+	void setSimilarities(int similarities);
+	float getSumProbs();
+	void setSumProbs(float sumProbs);
+	string& getTarget();
+	void setTarget(string& target);
+	string& getTargetSsPred();
+	void setTargetSsPred(string& targetSsPred);
+	string& getTargetConsensus();
+	void setTargetConsensus(string& targetConsensus);
+	int getTargetEnd();
+	void setTargetEnd(int targetEnd);
+	int getTargetStart();
+	void setTargetStart(int targetStart);
+	string& getQuerySsPred();
+	void setQuerySsPred(string& querySsPred);
+private:
+
+	string proteinName;
+	float probab;
+	float expect;
+	float score;
+	int aligned_cols;
+	int identities;
+	int similarities;
+	float sum_probs;
+	string query_ss_pred;
+	int queryStart;
+	string query;
+	int queryEnd;
+	string queryConsensus;
+	string alignment;
+	int targetStart;
+	int targetEnd;
+	string targetConsensus;
+	string target;
+	string target_ss_pred;
+	string confidence;
+};
+
+#endif /* HHRRESULT_H_ */
