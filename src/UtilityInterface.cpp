ad6a7c985cf6a5ba53130b913c71556cfb9e6c28
rafaelfang <cf797@mail.missouri.edu>
2015-05-21 00:05:07 -0500

    UtilityInterface.cpp

--- src/UtilityInterface.cpp
+++ src/UtilityInterface.cpp
@@ -65,9 +65,80 @@ void UtilityInterface::findProteinSeqLength() {
 	fgets(line, 500, fptr);
 	fgets(line, 500, fptr);
 
-	proteinSeqLength= strlen(line)-1;//should not count the "\0"
+	proteinSeqLength = strlen(line) - 1; //should not count the "\0"
 	//cout<<"proteinSeqLength"<<proteinSeqLength<<endl;
 	fclose(fptr);
 }
 
+string UtilityInterface::convertResidueName(const char residueName) {
+	if (residueName=='A') {
+		return "ALA";
+	}
+
+	if (residueName=='R') {
+		return "ARG";
+	}
+
+	if (residueName=='N') {
+		return "ASN";
+	}
+	if (residueName=='D') {
+		return "ASP";
+	}
+	if (residueName=='B') {
+		return "ASX";
+	}
+	if (residueName=='C') {
+		return "CYS";
+	}
+	if (residueName=='Q') {
+		return "GLN";
+	}
+	if (residueName=='E') {
+		return "GLU";
+	}
+	if (residueName=='Z') {
+		return "GLX";
+	}
+	if (residueName=='G') {
+		return "GLY";
+	}
+	if (residueName=='H') {
+		return "HIS";
+	}
+	if (residueName=='I') {
+		return "ILE";
+	}
+	if (residueName=='L') {
+		return "LEU";
+	}
+	if (residueName=='K') {
+		return "LYS";
+	}
+	if (residueName=='M') {
+		return "MET";
+	}
+	if (residueName=='F') {
+		return "PHE";
+	}
+	if (residueName=='P') {
+		return "PRO";
+	}
+	if (residueName=='S') {
+		return "SER";
+	}
+	if (residueName=='T') {
+		return "THR";
+	}
+	if (residueName=='W') {
+		return "TRP";
+	}
+	if (residueName=='Y') {
+		return "TYR";
+	}
+	if (residueName=='V') {
+		return "VAL";
+	}
+	return "INVALID NAME";
+}
 
