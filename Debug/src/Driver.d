9907c28c6143badb73179fbf1ce6a038e952603c
rafaelfang <cf797@mail.missouri.edu>
2015-07-09 22:09:14 -0500

    Driver.d

--- Debug/src/Driver.d
+++ Debug/src/Driver.d
@@ -1,7 +1,8 @@
 src/Driver.d: ../src/Driver.cpp ../src/BLANRImpl.h ../src/BLANRResult.h \
  ../src/AlignmentResult.h ../src/UtilityInterface.h ../src/BLAPDBImpl.h \
- ../src/BLAPDBResult.h ../src/HHRImpl.h ../src/HHRResult.h \
- ../src/HHRNRImpl.h ../src/HHRNRResult.h
+ ../src/BLAPDBResult.h ../src/CNFSearchImpl.h ../src/CNFSearchResult.h \
+ ../src/HHRImpl.h ../src/HHRResult.h ../src/HHRNRImpl.h \
+ ../src/HHRNRResult.h
 
 ../src/BLANRImpl.h:
 
@@ -14,6 +15,10 @@ src/Driver.d: ../src/Driver.cpp ../src/BLANRImpl.h ../src/BLANRResult.h \
 ../src/BLAPDBImpl.h:
 
 ../src/BLAPDBResult.h:
+
+../src/CNFSearchImpl.h:
+
+../src/CNFSearchResult.h:
 
 ../src/HHRImpl.h:
 
