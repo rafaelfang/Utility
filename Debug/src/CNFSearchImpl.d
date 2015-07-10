5c67a7734f39987b203e30e65f14092b8d8766a0
rafaelfang <cf797@mail.missouri.edu>
2015-07-09 22:08:50 -0500

    CNFSearchImpl.d

---
+++ Debug/src/CNFSearchImpl.d
@@ -0,0 +1,11 @@
+src/CNFSearchImpl.d: ../src/CNFSearchImpl.cpp ../src/CNFSearchImpl.h \
+ ../src/CNFSearchResult.h ../src/AlignmentResult.h \
+ ../src/UtilityInterface.h
+
+../src/CNFSearchImpl.h:
+
+../src/CNFSearchResult.h:
+
+../src/AlignmentResult.h:
+
+../src/UtilityInterface.h:
